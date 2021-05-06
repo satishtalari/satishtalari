42b85e74387dda3b1de4fc9a21f527a6663a5d4c
satishtalari <83686481+satishtalari@users.noreply.github.com>
2021-05-06 17:10:18 +0700

    Add files via upload

---
+++ day 2 class.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello Satish")
+
+
+# In[2]:
+
+
+a=5
+print(a)
+
+
+# In[3]:
+
+
+b=7
+print(b)
+
+
+# In[4]:
+
+
+print(a+b)
+
+
+# In[5]:
+
+
+c=a-b
+
+
+# In[6]:
+
+
+print(c)
+
+
+# In[7]:
+
+
+2n=9
+
+
+# In[8]:
+
+
+n a=11
+
+
+# In[9]:
+
+
+n2=3
+
+
+# In[10]:
+
+
+print(n2)
+
+
+# In[11]:
+
+
+$a=10
+
+
+# In[12]:
+
+
+a$=10
+
+
+# In[13]:
+
+
+name_one="satish"
+
+
+# In[14]:
+
+
+print(name_one)
+
+
+# In[15]:
+
+
+name="satish talari"
+print(name)
+
+
+# In[16]:
+
+
+print(name.title())
+
+
+# In[17]:
+
+
+print(name.upper())
+
+
+# In[18]:
+
+
+print(name.lower())
+
+
+# In[ ]:
+
+
+
+
